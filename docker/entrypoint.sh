30496c74d596f9fb846a71f11f6778f8ac8e6127
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-04-29 00:06:55 +0200

    resync

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+\#!/bin/bash
 
-HOMER_IP="${HONER_IP:-127.0.0.1}"
+HOMER_IP="${HOMER_IP:-127.0.0.1}"
 PUBLIC_IP="${PUBLIC_IP:-127.0.0.1}"
 
-sed -i 's/homer.seven/'"$HOMER_IP"'/g' /usr/src/hepsrc/config.js
-sed -i 's/PUBLIC_IP_HERE/'"$PUBLIC_IP"'/g' /usr/src/hepsrc/config.js
+sed -i "s/homer.seven/$HOMER_IP/g" /app/config.js
+sed -i "s/PUBLIC_IP_HERE/$PUBLIC_IP/g" /app/config.js
 
 exec "$@"
